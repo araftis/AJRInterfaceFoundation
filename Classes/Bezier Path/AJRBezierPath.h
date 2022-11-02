4e9d26e8aa15edea8ed211faddce68def7e748fd
AJ Raftis <araftis@calpoly.edu>
2022-11-01 23:38:40 -0700

    Updated copyright, including the script to generate them.

--- Classes/Bezier Path/AJRBezierPath.h
+++ Classes/Bezier Path/AJRBezierPath.h
@@ -28,31 +28,6 @@ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
 ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-/*%*%*%*%*
- Copyright (C) 1995-2004 A. J. Raftis
- 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
- 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
- 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- 
- Or, contact the author,
- 
- A. J. Raftis
- 359 Melanie Lane
- Nipomo, CA 93444
- alex@raftis.net
- 
- *%*%*%*%*/
 
 #import <CoreGraphics/CoreGraphics.h>
 
